2ea955b31109b3c57bcc21ff570ebf430eb7b163
Younes Yousfi <younesyousfipro@gmail.com>
2026-09-16 18:21:41 +0200

    chore: verouillage des dependances et nettoyage component d'essai

---
+++ src/app/component-name/component-name.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-name',
+  imports: [],
+  templateUrl: './component-name.component.html',
+  styleUrl: './component-name.component.css'
+})
+export class ComponentNameComponent {
+
+}
