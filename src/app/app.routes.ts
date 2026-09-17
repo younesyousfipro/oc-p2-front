461f76c8b41bd7128bf57c44ffd45fe8d3f8e43b
Younes Yousfi <younesyousfipro@gmail.com>
2026-09-17 21:05:02 +0200

    feat: ecran de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from './pages/register/register.component';
 import {AppComponent} from './app.component';
+import {LoginComponent} from './pages/login/login.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,11 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  // task3 - route de l'ecran de login
+  {
+    path: 'login',
+    component: LoginComponent
   }
 
 ];
