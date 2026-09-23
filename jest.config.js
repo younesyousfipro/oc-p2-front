a67fe6b84064bd28c185d0e716e3805a62dde499
Younes Yousfi <younesyousfipro@gmail.com>
2026-09-23 15:59:43 +0200

    chore: ajustement de jest.config (tableau en console)

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
   testMatch: ['**/+(*.)+(spec).+(ts|js)'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   collectCoverage: true,
-  coverageReporters: ['html'],
+  // Sans collectCoverageFrom, Jest ne mesure que les fichiers importes par un test :
+  // tout fichier sans spec est absent du rapport au lieu d'y figurer a 0 %.
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.spec.ts',
+  ],
+  // 'text' affiche le tableau en console, sans avoir a ouvrir le rapport HTML.
+  coverageReporters: ['html', 'text'],
 };
